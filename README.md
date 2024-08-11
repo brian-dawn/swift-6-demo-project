ab19b6b5537df8fe44b6e85d1ec1381eb53de506
Brian Dawn <brian.t.dawn@gmail.com>
2024-08-11 10:47:27 -0500

    Initialize the repo.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Basics
+
+# To init
+
+```bash
+swift package init --type executable
+```
+
+
+# To run
+
+```bash
+swift build
+swift run
+```
---
+++ Sources/main.swift
@@ -0,0 +1,10 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+print("Hello, world!")
+
+func helloworld(name: String) {
+    print("Hello, \(name)!")
+}
+
+helloworld(name: "Swift")
